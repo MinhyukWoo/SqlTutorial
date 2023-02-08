9694277ca700d25a34258dc290699ed7941915f3
MinhyukWoo <84601150+MinhyukWoo@users.noreply.github.com>
2023-02-08 14:58:33 +0900

    Update InitTOrder.sql

--- Init/InitTOrder.sql
+++ Init/InitTOrder.sql
@@ -1,4 +1,4 @@
--- Oracle
+-- Only Oracle Start
 CREATE TABLE tOrder
 (
   orderID INT GENERATED AS IDENTITY PRIMARY KEY,	-- 주문 번호
@@ -10,9 +10,10 @@ CREATE TABLE tOrder
   remark VARCHAR2(1000) NULL			-- 메모 사항
 );
 
-INSERT INTO tOrder (member,item,orderDate,num,status) VALUES ('춘향','청바지','2019-12-3',3,2);
-INSERT INTO tOrder (member,item,orderDate,num,status) VALUES ('향단','대추','2019-12-4',10,1);
-INSERT INTO tOrder (member,item,orderDate,num,status) VALUES ('방자','전자담배','2019-12-2',4,1);
-INSERT INTO tOrder (member,item,orderDate,num,status) VALUES ('향단','사과','2019-12-5',5,2);
-INSERT INTO tOrder (member,item,orderDate,num,status) VALUES ('흥부','노트북','2019-12-5',2,1);
-INSERT INTO tOrder (member,item,orderDate,num,status) VALUES ('방자','핸드폰','2019-11-1',1,3);
+INSERT INTO tOrder (member,item,orderDate,num,status) VALUES ('춘향','청바지',TO_DATE('2019-12-3', 'YYYY-MM-DD'),3,2);
+INSERT INTO tOrder (member,item,orderDate,num,status) VALUES ('향단','대추',TO_DATE('2019-12-4', 'YYYY-MM-DD'),10,1);
+INSERT INTO tOrder (member,item,orderDate,num,status) VALUES ('방자','전자담배',TO_DATE('2019-12-2', 'YYYY-MM-DD'),4,1);
+INSERT INTO tOrder (member,item,orderDate,num,status) VALUES ('향단','사과',TO_DATE('2019-12-5', 'YYYY-MM-DD'),5,2);
+INSERT INTO tOrder (member,item,orderDate,num,status) VALUES ('흥부','노트북',TO_DATE('2019-12-5', 'YYYY-MM-DD'),2,1);
+INSERT INTO tOrder (member,item,orderDate,num,status) VALUES ('방자','핸드폰',TO_DATE('2019-11-1', 'YYYY-MM-DD'),1,3);
+-- Only Oracle End
